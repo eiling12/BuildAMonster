371ceea04b5c031a82aa78454ae4dfcbdff0f532
eiling12 <167204559+eiling12@users.noreply.github.com>
2024-04-20 21:56:35 -0700

    Update Monster.js

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -20,6 +20,8 @@ class Monster extends Phaser.Scene {
         
         // update instruction text
         document.getElementById('description').innerHTML = '<h2>Monster.js<br>S - smile // F - show fangs<br>A - move left // D - move right</h2>'
+
+        
     }
 
     create() {
@@ -32,6 +34,17 @@ class Monster extends Phaser.Scene {
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.arm1 = this.add.sprite(this.bodyX + 100, this.bodyY + 50, "monsterParts", "arm_greenD.png");
+        my.sprite.arm2 = this.add.sprite(this.bodyX - 100, this.bodyY + 50, "monsterParts", "arm_greenD.png").setFlipX(true);
+        my.sprite.leg1 = this.add.sprite(this.bodyX + 50, this.bodyY + 120, "monsterParts", "leg_greenD.png");
+        my.sprite.leg2 = this.add.sprite(this.bodyX - 50, this.bodyY + 120, "monsterParts", "leg_greenD.png").setFlipX(true);
+        my.sprite.eye = this.add.sprite(this.bodyX , this.bodyY - 20, "monsterParts", "eye_red.png");
+        my.sprite.mouthSmile = this.add.sprite(this.bodyX , this.bodyY + 40, "monsterParts", "mouthA.png");
+        my.sprite.mouthFang = this.add.sprite(this.bodyX , this.bodyY + 40, "monsterParts", "mouthB.png");
+        my.sprite.horn1 = this.add.sprite(this.bodyX + 50, this.bodyY - 70, "monsterParts", "detail_green_horn_large.png");
+        my.sprite.horn2 = this.add.sprite(this.bodyX - 50, this.bodyY - 70, "monsterParts", "detail_green_horn_large.png").setFlipX(true);
+
+        my.sprite.mouthFang.visible = false;
 
         
     }
@@ -39,7 +52,30 @@ class Monster extends Phaser.Scene {
     update() {
         let my = this.my;    // create an alias to this.my for readability
 
-       
-    }
+        // Smile and Fang
+        this.input.keyboard.on('keydown-S', function(event) {
+            my.sprite.mouthFang.visible = false;
+            my.sprite.mouthSmile.visible = true;
+        });
 
+        this.input.keyboard.on('keydown-F', function(event) {
+            my.sprite.mouthFang.visible = true;
+            my.sprite.mouthSmile.visible = false;
+        });
+
+
+        // Movement
+
+        if (this.input.keyboard.addKey('A').isDown) {
+            for (let part in my.sprite){
+                my.sprite[part].x -= 1;
+            } 
+        }
+
+        if (this.input.keyboard.addKey('D').isDown) {
+            for (let part in my.sprite) {
+                my.sprite[part].x += 1;
+            }
+        }     
+    }
 }
